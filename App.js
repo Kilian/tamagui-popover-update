db595225caed979161b3e7f21440edf54a6d9efd
Kilian Valkhof <kilian@kilianvalkhof.com>
2025-01-22 10:31:39 +0100

    Add animated workaround

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import { config } from "@tamagui/config"
-import { TamaguiProvider, createTamagui } from "tamagui"
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { defaultConfig } from "@tamagui/config/v4";
+import { TamaguiProvider, createTamagui } from "tamagui";
+import { createAnimations } from "@tamagui/animations-moti";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 // or any files within the Snack
-import AssetExample from './components/AssetExample';
+import AssetExample from "./components/AssetExample";
 
+const animations = createAnimations({
+  bouncy: {
+    type: "spring",
+    damping: 9,
+    mass: 0.9,
+    stiffness: 150,
+  },
+  lazy: {
+    type: "spring",
+    damping: 18,
+    stiffness: 50,
+  },
+  slow: {
+    type: "spring",
+    damping: 15,
+    stiffness: 40,
+  },
+  quick: {
+    type: "spring",
+    damping: 50,
+    mass: 0.5,
+    stiffness: 250,
+  },
+  tooltip: {
+    type: "timing",
+    damping: 10,
+    mass: 0.9,
+    stiffness: 100,
+  },
+});
 
-const tamaguiConfig = createTamagui(config)
+const tamaguiConfig = createTamagui({
+  ...defaultConfig,
+  animations,
+});
 
 export default function App() {
   return (
     <TamaguiProvider config={tamaguiConfig}>
-    <SafeAreaView style={styles.container}>
-      <AssetExample />
-    </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <AssetExample />
+      </SafeAreaView>
     </TamaguiProvider>
   );
 }
@@ -21,9 +55,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: '#ddd',
+    justifyContent: "flex-end",
+    backgroundColor: "#ddd",
     padding: 8,
   },
-
 });
--- components/AssetExample.js
+++ components/AssetExample.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 import { Popover, Accordion } from "tamagui";
 
 export default function AssetExample() {
+  const [flag, setFlag] = useState(false);
+
+  function handleValueChange() {
+    const interval = setInterval(() => {
+      setFlag((prev) => !prev);
+    }, 8);
+
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 320);
+  }
+
   return (
     <View style={styles.container}>
-      <Popover placement="right-start" stayInFrame>
+      <Popover placement="right-start" stayInFrame allowFlip={flag}>
         <Popover.Trigger>
           <View>
             <Text>Open Popover</Text>
@@ -14,19 +27,21 @@ export default function AssetExample() {
         <Popover.Content backgroundColor="white">
           <Popover.Arrow size="$4" />
           <Text>Some other content</Text>
-          <Accordion type="single" collapsible backgroundColor="white">
+          <Accordion type="single" collapsible backgroundColor="white" onValueChange={handleValueChange}>
             <Accordion.Item value="a">
               <Accordion.Trigger backgroundColor="white">
                 <Text>Open the accordion</Text>
               </Accordion.Trigger>
-              <Accordion.Content backgroundColor="white">
-                <Text>Some other content</Text>
-                <Text>Some other content</Text>
-                <Text>Some other content</Text>
-                <Text>Some other content</Text>
-                <Text>Some other content</Text>
-                <Text>Some other content</Text>
-              </Accordion.Content>
+              <Accordion.HeightAnimator animation="quick">
+                <Accordion.Content animation="quick" backgroundColor="white" overflow="hidden" marginBottom={30}>
+                  <Text>Some other content</Text>
+                  <Text>Some other content</Text>
+                  <Text>Some other content</Text>
+                  <Text>Some other content</Text>
+                  <Text>Some other content</Text>
+                  <Text>Some other content</Text>
+                </Accordion.Content>
+              </Accordion.HeightAnimator>
             </Accordion.Item>
           </Accordion>
         </Popover.Content>
